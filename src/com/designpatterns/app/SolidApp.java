9715b634aa2b3f0e70955ec9d836297cff15ecc8
devjosegomez <devjosegomez@gmail.com>
2019-10-22 20:37:59 -0500

    Added comment about the description of Solid Principle

--- src/com/designpatterns/app/SolidApp.java
+++ src/com/designpatterns/app/SolidApp.java
@@ -1,5 +1,21 @@
 package com.designpatterns.app;
 
+/*
+	---		Single responsible principle		---
+	A software entity (class, method) should have only one reason to change
+	if a class or a method does more than one procedure, it is advisable to
+	separate it into two distinct classes / methods
+	it can be done easily with the help of Interfaces (able)
+	
+	So if we have 2 reasons to change for a class then 
+	we should split the functionality into two separate classes
+		Note: Each class handle a single responsibility!
+	
+	it leads to a low coupled design with less and lighter dependencies.
+
+*/
+
+
 public class SolidApp {
 
 	public static void main(String[] args) {
